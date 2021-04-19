c8feaa16b924517288035aab8ab253be9fb0f274
Michele Beretta <beretta.m98@gmail.com>
2021-04-19 17:57:56 +0200

    added some simple simulation

--- lib/bb84.ex
+++ lib/bb84.ex
@@ -18,11 +18,16 @@ defmodule BB84 do
   """
   def decode(qs, bs) when is_list(qs) and is_list(bs) do
     Enum.zip(qs, bs)
+    |> Enum.map(&decode_qubit/1)
+  end
+
+  def measure(qs, bs) when is_list(qs) and is_list(bs) do
+    Enum.zip(qs, bs)
     |> Enum.map(&measure_qubit/1)
   end
 
   @doc """
-  Keeps only the qubits in `qs` measured with the same bases (`bs1` and `bs2` are the two bases' lists)
+  Keeps only the qubits (or bits) in `qs` measured with the same bases (`bs1` and `bs2` are the two bases' lists)
   """
   def discard_different_bases(qs, bs1, bs2) do
     Enum.zip([qs, bs1, bs2])
@@ -45,6 +50,9 @@ defmodule BB84 do
   defp encode_qubit({0, 1}), do: elem(@bases2, 0)
   defp encode_qubit({1, 1}), do: elem(@bases2, 1)
 
-  defp measure_qubit({q, 0}), do: Qubit.to_bit(q, @bases1)
-  defp measure_qubit({q, 1}), do: Qubit.to_bit(q, @bases2)
+  defp measure_qubit({q, 0}), do: Qubit.measure(q, @bases1)
+  defp measure_qubit({q, 1}), do: Qubit.measure(q, @bases2)
+
+  defp decode_qubit({q, 0}), do: Qubit.to_bit(q, @bases1)
+  defp decode_qubit({q, 1}), do: Qubit.to_bit(q, @bases2)
 end
