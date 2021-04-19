29936d1033fe305ca53c94e137a652d014d39deb
Michele Beretta <beretta.m98@gmail.com>
2021-04-19 16:58:13 +0200

    added different bases check

--- lib/bb84.ex
+++ lib/bb84.ex
@@ -1,6 +1,9 @@
 defmodule BB84 do
   @bases1 {Qubit.new(), Qubit.new() |> Qubit.qnot()}
-  @bases2 {Qubit.new() |> Qubit.hadamard(), Qubit.new() |> Qubit.qnot() |> Qubit.hadamard()}
+  @bases2 {
+    Qubit.new() |> Qubit.hadamard(),
+    Qubit.new() |> Qubit.qnot() |> Qubit.hadamard()
+  }
 
   @doc """
   Encodes a list `as` of 0s and 1s as a list of qubit, using a second list `bs`
@@ -16,6 +19,16 @@ defmodule BB84 do
   def decode(qs, bs) when is_list(qs) and is_list(bs) do
     Enum.zip(qs, bs)
     |> Enum.map(&measure_qubit/1)
+  end
+
+  @doc """
+  Keeps only the qubits in `qs` measured with the same bases (`bs1` and `bs2` are the two bases' lists)
+  """
+  def discard_different_bases(qs, bs1, bs2) do
+    Enum.zip([qs, bs1, bs2])
+    |> Enum.filter(fn {_, b1, b2} -> b1 == b2 end)
+    |> Enum.map(fn {q, b, _} -> {q, b} end)
+    |> Enum.unzip()
   end
 
   defp encode_qubit({0, 0}), do: elem(@bases1, 0)
--- test/bb84_test.exs
+++ test/bb84_test.exs
@@ -33,4 +33,22 @@ defmodule BB84Test do
     assert Qubit.prob(q4, 0) > 0.5 - 1 / 1_000_000 && Qubit.prob(q4, 0) < 0.5 + 1 / 1_000_000
     assert Qubit.prob(q4, 1) > 0.5 - 1 / 1_000_000 && Qubit.prob(q4, 1) < 0.5 + 1 / 1_000_000
   end
+
+  test "it can check against different bases used" do
+    as = [0, 1, 0, 1, 0, 0, 1, 1]
+    b1 = [0, 0, 1, 1, 1, 1, 0, 0]
+    b2 = [0, 1, 1, 0, 0, 1, 0, 1]
+
+    qs = BB84.encode(as, b1)
+    qs_decoded = BB84.decode(qs, b2)
+
+    {qs1, common_bases1} = BB84.discard_different_bases(qs, b1, b2)
+    {qs2, common_bases2} = BB84.discard_different_bases(qs_decoded, b1, b2)
+
+    assert common_bases1 == common_bases2
+    assert length(qs1) <= length(as)
+    assert length(qs2) <= length(as)
+
+    assert qs1 == qs2
+  end
 end
